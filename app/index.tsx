786f716723a10a4cbdb5f93f1127ba413076a870
chenping <chenping@jingyoutech.com>
2020-08-27 10:13:24 +0800

    test

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
+import fs from 'fs';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 import { history, configuredStore } from './store';
 import './app.global.css';
@@ -7,6 +8,22 @@ import './app.global.css';
 const store = configuredStore();
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
+
+try {
+  /**
+   * Works here
+   * require module like npm packge but absolute path
+   */
+  const result = require('c:/app/');
+
+  /**
+   * Not work
+   */
+  // const result = require('./app.asar/index.js');
+  console.log(result);
+} catch (error) {
+  console.log(error);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line global-require
